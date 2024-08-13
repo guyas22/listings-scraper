01e62cf58f2b9850b4698ff21cc5203944e1fb3b
asulinguy <guyas22@gmail.com>
2024-08-14 02:37:01 +0300

    fix

--- scraper.py
+++ scraper.py
@@ -28,32 +28,28 @@ class Scraper:
         browser = None
         try:
             transformed_url = self.transform_url(original_url)
-            id_part = original_url.split('/')[-1]
-            
             async with async_playwright() as p:
                 browser = await p.chromium.launch(headless=headless)
                 context = await browser.new_context(java_script_enabled=True)
                 page = await context.new_page()
 
-                # Apply stealth plugin
-                await stealth_async(page)
                 await page.goto(transformed_url)
-
-                # Check if the ID is present in the URL
                 current_url = page.url
-                if id_part not in current_url:
+                if "ID" not in current_url:
                     self.logger.warning(f"ID not found in URL: {current_url}, skipping...")
                     return None
 
-                # Take a screenshot of the full page immediately
                 await page.screenshot(path=output_file, full_page=True)
                 self.logger.info(f"Screenshot saved to {output_file}")
+                return self.encode_image(output_file)
+
         except Exception as e:
-            self.logger.error(f"An error occurred: {e}")
+            self.logger.error(f"An error occurred during screenshot capture: {e}")
+        
         finally:
             if browser:
                 await browser.close()
-        return self.encode_image(output_file)
+
 
     def encode_image(self, image_path):
         try:
